995d32301d669b763d0b12ec2c66dda737000441
Brian Yip <itsbriany@gmail.com>
2016-09-24 15:13:58 -0400

    Can now filter users by tags

--- test/rabbit_test.js
+++ test/rabbit_test.js
@@ -26,9 +26,49 @@ suite('Rabbit Wrapper', () => {
   test('deleteUserException', async () => {
     try {
       await helper.rabbit.deleteUser('not a user');
-      expect(true).to.be.false;
+      assert.equal(true, false);
     } catch (error) {
-      expect(error.statusCode).to.equal(404);
+      assert.equal(error.statusCode, 404);
     }
+  });
+
+  test('users', async () => {
+    let usersList = await helper.rabbit.users();
+    assert(usersList instanceof Array);
+    assert(_.has(usersList[0], 'name'));
+    assert(_.has(usersList[0], 'password_hash'));
+    assert(_.has(usersList[0], 'hashing_algorithm'));
+    assert(_.has(usersList[0], 'tags'));
+  });
+
+  test('_filterUsersWithAllTags', () => {
+    const user1 = { tags: 'foo,bar' };
+    const user2 = { tags: 'foo' };
+    const user3 = { tags: 'bar' };
+    const user4 = { tags: 'bar,foo' };
+
+    const users = [ user1, user2, user3, user4 ];
+    const tags = ['foo', 'bar'];
+
+    let usersWithAllTags = helper.rabbit._filterUsersWithAllTags(users, tags);
+    assert(usersWithAllTags.length === 2);
+    assert(_.find(usersWithAllTags, { tags: 'foo,bar' }));
+    assert(_.find(usersWithAllTags, { tags: 'bar,foo' }));
+  });
+
+  test('_filterUsersWithAnyTags', () => {
+    const user1 = { tags: 'foo,bar' };
+    const user2 = { tags: 'foo' };
+    const user3 = { tags: 'bar' };
+    const user4 = { tags: 'bar,foo' };
+    const user5 = { tags: 'car, foo' };
+
+    const users = [ user1, user2, user3, user4, user5 ];
+    const tags = ['bar', 'car'];
+
+    let usersWithAnyTags = helper.rabbit._filterUsersWithAnyTags(users, tags);
+    assert(usersWithAnyTags.length === 4);
+    assert.equal(usersWithAnyTags.filter(user => user.tags.includes('bar')).length, 3);
+    assert.equal(usersWithAnyTags.filter(user => user.tags.includes('car')).length, 1);
   });
 });
