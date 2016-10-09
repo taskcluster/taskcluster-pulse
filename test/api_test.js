35cc1b3cdb45ffa7fa4390f0feb3752006073351
Alex Urda <m_urda@hotmail.com>
2016-10-09 12:38:08 -0400

    add payload to namespace notification request

--- test/api_test.js
+++ test/api_test.js
@@ -32,8 +32,12 @@ suite('API', () => {
   test('namespace', () => {
     return helper.pulse.namespace('samplenamespace', {
       contact: {
-        method: 'irc',
-        id:     'ircusername',
+        method: 'email',
+        payload: {
+          address: 'a@a.com',
+          subject: 'subject',
+          content: 'content',
+        },
       },
     });
   });
